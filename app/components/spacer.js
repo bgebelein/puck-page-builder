6a5fdf848781de903fe4602a951f2a2c2ae524eb
bgebelein <bent.gebelein@akquinet.de>
2025-08-12 17:38:58 +0200

    Simplified spacer component + UI

--- app/components/spacer.js
+++ app/components/spacer.js
@@ -1,59 +1,59 @@
 import { Icon } from "@iconify/react";
+import { FieldLabel } from "@measured/puck";
 
-const spacerWidths = {
-    0: "w-0",
-    2: "w-2",
-    4: "w-4",
-    8: "w-8",
-    20: "w-20",
-    30: "w-30",
-    40: "w-40",
-}
-
-const spacerHeights = {
-    0: "h-0",
-    2: "h-2",
-    4: "h-4",
-    8: "h-8",
-    20: "h-20",
-    30: "h-30",
-    40: "h-40",
+const spacings = {
+    1: "size-4",
+    2: "size-8",
+    3: "size-20",
+    4: "size-30",
+    5: "size-40",
 }
 
 export const Spacer = {
     label: "Spacer",
     fields: {
-        direction: {
-            type: "radio",
-            label: "Direction",
-            labelIcon: <Icon icon="lucide:move" />,
-            options: [
-                { label: "Horizontal", value: "w" },
-                { label: "Vertical", value: "h" },
-            ],
-        },
+        // size: {
+        //     label: "Size",
+        //     labelIcon: <Icon icon="lucide:ruler" />,
+        //     type: "select",
+        //     options: [
+        //         { label: "None", value: "0" },
+        //         { label: "XS", value: "2" },
+        //         { label: "SM", value: "4" },
+        //         { label: "MD", value: "8" },
+        //         { label: "LG", value: "20" },
+        //         { label: "XL", value: "30" },
+        //         { label: "2XL", value: "40" },
+        //     ],
+        // },
         size: {
+            type: "custom",
             label: "Size",
-            labelIcon: <Icon icon="lucide:ruler" />,
-            type: "select",
-            options: [
-                { label: "None", value: "0" },
-                { label: "XS", value: "2" },
-                { label: "SM", value: "4" },
-                { label: "MD", value: "8" },
-                { label: "LG", value: "20" },
-                { label: "XL", value: "30" },
-                { label: "2XL", value: "40" },
-            ],
+            min: 1,
+            max: 5,
+            step: 1,
+            render: ({ field, name, onChange, value }) => (
+                <FieldLabel label={field.label}>
+                    <input
+                        type="range"
+                        min={field.min}
+                        max={field.max}
+                        step={field.step}
+                        defaultValue={value}
+                        name={name}
+                        onChange={(e) => onChange(e.currentTarget.value)}
+                        style={{ border: "1px solid black", padding: 4 }}
+                    />
+                </FieldLabel>
+            ),
         },
     },
     defaultProps: {
-        direction: "h",
-        size: "8",
+        size: 3,
     },
     render: ({ direction, size }) => {
         return (
-            <div className={`${direction}-${size}`}></div>
+            <div className={spacings[size]}></div>
         );
     },
 }
