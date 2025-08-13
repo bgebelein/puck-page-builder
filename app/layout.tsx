4ca574d1242f1cc200751042fa4bf37988a10cbf
bgebelein <bent.gebelein@akquinet.de>
2025-08-13 15:06:27 +0200

    Added basic meta elements

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,8 @@ export default function RootLayout({
   return (
     <html lang="de">
       <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,400;1,700&family=Poppins:ital,wght@0,600;1,600&display=swap" rel="stylesheet" />
